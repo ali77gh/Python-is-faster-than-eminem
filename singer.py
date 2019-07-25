12d92928bcca37ef7e2126d332ad0e42113b62e8
ali <alighahremani1377@gmail.com>
2019-07-25 21:43:48 +0430

    upload files

---
+++ singer.py
@@ -0,0 +1,26 @@
+#ememem record
+# 16 s
+
+import time
+import pyttsx3
+    
+engine = pyttsx3.init()
+
+path = './fastpart.txt'
+f = open(path, 'r')
+rapGod = f.read()
+
+# improve speed
+rapGod = rapGod.replace('\n',' ')
+
+print("raping...")
+
+engine.setProperty('rate', 300)  # words per minute
+engine.say(rapGod)
+
+start = time.time()
+engine.runAndWait()
+end = time.time()
+
+print("python: " + str(end-start)[0:4] + "s")
+print("eminem: 16s")
